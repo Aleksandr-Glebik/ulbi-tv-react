c48e7a326715d431a1902ef4c59ce9a98a9e0f49
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-04 15:39:29 +0300

    feat: add func component Counter and classComponent ClassCounter

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
+import ClassCounter from './components/ClassCounter'
+import Counter from './components/Counter'
 
 function App() {
 
-  const [likes, setLikes] = useState(0)
-  const [value, setValue] = useState('Текст внутри инпута')
-
-  function increment() {
-    setLikes(likes + 1)
-  }
-  function decrement() {
-    setLikes(likes - 1)
-  }
-
   return (
     <div className='App'>
-       <h2>{likes}</h2>
-       <button onClick={ increment }>Increment</button>
-       <button onClick={ decrement }>Decrement</button>
-       <hr />
-       <input
-         type={'text'}
-         value={value}
-         onChange={ event => setValue(event.target.value)}
-        />
-       <h1>{value}</h1>
+      <Counter />
+      <hr />
+      <ClassCounter />
     </div>
   )
 }
