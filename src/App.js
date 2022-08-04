d123a88669493c501f959b874be572d54bb07dcc
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-04 15:17:45 +0300

    feat: add input and setValue in input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 function App() {
 
   const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('Текст внутри инпута')
 
   function increment() {
     setLikes(likes + 1)
@@ -13,9 +14,16 @@ function App() {
 
   return (
     <div className='App'>
-      <h2>{likes}</h2>
+       <h2>{likes}</h2>
        <button onClick={ increment }>Increment</button>
        <button onClick={ decrement }>Decrement</button>
+       <hr />
+       <input
+         type={'text'}
+         value={value}
+         onChange={ event => setValue(event.target.value)}
+        />
+       <h1>{value}</h1>
     </div>
   )
 }
