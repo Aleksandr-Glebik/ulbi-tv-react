cfd91b9776683b7f614e3eef046902c6776a7a9e
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-05 15:24:41 +0300

    feat: add component MySelect and func sortPosts

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react'
 import './styles/App.css'
 import PostList from './components/PostList'
 import PostForm from './components/PostForm'
+import MySelect from './components/UI/select/MySelect'
 
 function App() {
 
   const [posts, setPosts] = useState([
-    {id: 1, title: 'HTML', body: 'Description'},
-    {id: 2, title: 'CSS', body: 'Description'},
-    {id: 3, title: 'Javascript', body: 'Description'},
+    {id: 1, title: 'HTML', body: 'Markup'},
+    {id: 2, title: 'CSS', body: 'Styles'},
+    {id: 3, title: 'Javascript', body: 'Functionality'},
   ])
+
+  const [selectedSort, setSelectedSort] = useState('')
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -19,10 +22,27 @@ function App() {
     setPosts(posts.filter( p => p.id !== post.id))
   }
 
+  const sortPosts = (sort) => {
+    // console.log('sort', sort)
+    setSelectedSort(sort)
+    setPosts([...posts].sort( (a, b) => a[sort].localeCompare(b[sort]) ))
+  }
+
   return (
     <div className='App'>
       <PostForm create={createPost}/>
-
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue={'Сортировка'}
+          options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'}
+          ]}
+        />
+      </div>
       {posts.length !== 0
         ? <PostList remove={removePost} posts={posts} title='Список постов 1'/>
         : <div style={{textAlign: 'center'}}><h1>Посты не были найдены!!!</h1></div>
