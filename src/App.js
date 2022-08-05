9413655740dbfdd0d816895800e23f89d0aa000c
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-05 12:16:41 +0300

    feat: add component button and input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './styles/App.css'
 import PostList from './components/PostList'
+import MyButton from './components/UI/button/MyButton'
+import MyInput from './components/UI/input/MyInput'
 
 function App() {
 
@@ -10,8 +12,26 @@ function App() {
     {id: 3, title: 'Javascript', body: 'Description'},
   ])
 
+  const [title, setTitle] = useState('')
+
+  const addNewPost = (event) => {
+    event.preventDefault()
+    console.log('title', title);
+  }
+
   return (
     <div className='App'>
+      <form>
+        {/* управляемый компонент */}
+        <MyInput
+          type={'text'}
+          placeholder='Название поста'
+          value={title}
+          onChange={ event => setTitle(event.target.value)}
+        />
+        <MyInput type={'text'} placeholder='Описание поста'/>
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
       <PostList posts={posts} title='Список постов 1'/>
     </div>
   )
