b04bcbf770499bd8f06c3292105e63fa7327f6c7
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-07 02:39:35 +0300

    feat: add pagination

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MyModal from './components/UI/MyModal/MyModal'
 import MyButton from './components/UI/button/MyButton'
 import { usePosts } from './hooks/usePosts'
 import Loader from './components/UI/Loader/Loader'
+import { getPageCount, getPagesArray } from './utils/pages'
 
 import PostService from './API/PostService'
 import { useFetching } from './hooks/useFetching'
@@ -17,12 +18,22 @@ function App() {
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-  // const [isPostLoading, setIsPostsLoading] = useState(false)
+  const [totalPages, setTotalPages] = useState(0)
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(1)
+
+  let pagesArray = getPagesArray(totalPages)
+  console.log('pagesArray', pagesArray);
 
   const [fetchPosts, isPostLoading, postError] = useFetching( async () => {
-    const posts = await PostService.getAll()
-    setPosts(posts)
+    const response = await PostService.getAll(limit, page)
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count']
+    console.log('x-total-count', response.headers['x-total-count']);
+    setTotalPages(getPageCount(totalCount, limit))
   })
+
+  console.log('totalPages', totalPages);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -31,10 +42,14 @@ function App() {
 
   useEffect( () => {
     fetchPosts()
-  }, [])
+  }, [page])
 
   const removePost = (post) => {
     setPosts(posts.filter( p => p.id !== post.id))
+  }
+
+  const changePage = (page) => {
+    setPage(page)
   }
 
   return (
@@ -69,6 +84,22 @@ function App() {
             title='Список постов 1'
           />
       }
+      <div className='page__wrapper'>
+        {pagesArray.map( p =>
+          <span
+            key={p}
+            className={
+              page === p
+               ? 'page page__current'
+               : 'page'
+            }
+            onClick={() => changePage(p)}
+            >
+            {p}
+          </span>
+        )}
+      </div>
+
 
 
     </div>
