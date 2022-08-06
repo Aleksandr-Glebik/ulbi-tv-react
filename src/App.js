65dc66f117615bcd9bf61875eec24c7eef615fd2
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-07 00:09:21 +0300

    feat: add axios and func fetchPosts

--- src/App.js
+++ src/App.js
@@ -6,14 +6,11 @@ import PostFilter from './components/PostFilter'
 import MyModal from './components/UI/MyModal/MyModal'
 import MyButton from './components/UI/button/MyButton'
 import { usePosts } from './hooks/usePosts'
+import axios from 'axios'
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'HTML', body: 'Markup'},
-    {id: 2, title: 'CSS', body: 'Styles'},
-    {id: 3, title: 'Javascript', body: 'Functionality'},
-  ])
+  const [posts, setPosts] = useState([])
 
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
@@ -24,12 +21,20 @@ function App() {
     setModal(false)
   }
 
+  async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    // console.log('response.data', response.data);
+    // console.log('response', response);
+    setPosts(response.data)
+  }
+
   const removePost = (post) => {
     setPosts(posts.filter( p => p.id !== post.id))
   }
 
   return (
     <div className='App'>
+      <button onClick={fetchPosts}>Получить посты</button>
       <MyButton style={{marginTop: '30px'}} onClick={ () => setModal(true)}>
         Создать пользователя
       </MyButton>
