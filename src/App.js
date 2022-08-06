3dc129eb9b8e3c75c74439b46e02f3acfd443ed1
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-07 02:49:47 +0300

    feat: add new component Pagination

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import MyModal from './components/UI/MyModal/MyModal'
 import MyButton from './components/UI/button/MyButton'
 import { usePosts } from './hooks/usePosts'
 import Loader from './components/UI/Loader/Loader'
-import { getPageCount, getPagesArray } from './utils/pages'
+import { getPageCount } from './utils/pages'
 
 import PostService from './API/PostService'
 import { useFetching } from './hooks/useFetching'
+import Pagination from './components/UI/pagination/Pagination'
 
 function App() {
 
@@ -22,10 +23,7 @@ function App() {
   const [limit, setLimit] = useState(10)
   const [page, setPage] = useState(1)
 
-  let pagesArray = getPagesArray(totalPages)
-  console.log('pagesArray', pagesArray);
-
-  const [fetchPosts, isPostLoading, postError] = useFetching( async () => {
+  const [fetchPosts, isPostLoading, postError] = useFetching( async (limit, page) => {
     const response = await PostService.getAll(limit, page)
     setPosts(response.data)
     const totalCount = response.headers['x-total-count']
@@ -33,15 +31,13 @@ function App() {
     setTotalPages(getPageCount(totalCount, limit))
   })
 
-  console.log('totalPages', totalPages);
-
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
     setModal(false)
   }
 
   useEffect( () => {
-    fetchPosts()
+    fetchPosts(limit, page)
   }, [page])
 
   const removePost = (post) => {
@@ -50,6 +46,7 @@ function App() {
 
   const changePage = (page) => {
     setPage(page)
+    fetchPosts(limit, page)
   }
 
   return (
@@ -84,23 +81,12 @@ function App() {
             title='Список постов 1'
           />
       }
-      <div className='page__wrapper'>
-        {pagesArray.map( p =>
-          <span
-            key={p}
-            className={
-              page === p
-               ? 'page page__current'
-               : 'page'
-            }
-            onClick={() => changePage(p)}
-            >
-            {p}
-          </span>
-        )}
-      </div>
 
-
+      <Pagination
+        totalPages={totalPages}
+        page={page}
+       changePage={changePage}
+      />
 
     </div>
   )
