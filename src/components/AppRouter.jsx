5dbab183161969543aadbd5c3b39e42b175169db
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-08 17:11:12 +0300

    feat: add comp Login, add privateRoutes and publicRoutes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,15 +1,18 @@
 import React from "react"
 import { Routes, Route } from "react-router-dom"
-import About from "../pages/About"
-import Posts from "../pages/Posts"
-import Error from "../pages/Error"
-import PostIdPage from '../pages/PostIdPage'
-import Home from "../pages/Home"
+// import About from "../pages/About"
+// import Posts from "../pages/Posts"
+// import Error from "../pages/Error"
+// import PostIdPage from '../pages/PostIdPage'
+// import Home from "../pages/Home"
+import { privateRoutes, publicRoutes } from "../router/index"
 
 const AppRouter = () => {
+    let isAuth = true
+    
     return (
         <Routes>
-            <Route path='/' element={<Home />} />
+            {/* <Route path='/' element={<Home />} />
 
             <Route path='posts' element={<Posts />} />
             <Route path='posts'>
@@ -17,7 +20,26 @@ const AppRouter = () => {
             </Route>
 
             <Route path='about' element={<About />} />
-            <Route path="*" element={<Error />} />
+            <Route path="*" element={<Error />} /> */}
+            {
+                isAuth
+                    ? privateRoutes.map( (route, ind) =>
+                        <Route
+                          key={ind}
+                          path={route.path}
+                          element={route.element}
+                          exact={route.exact}
+                        />
+                      )
+                    : publicRoutes.map( (route, ind) =>
+                        <Route
+                            key={ind}
+                            path={route.path}
+                            element={route.element}
+                            exact={route.exact}
+                        />
+                      )
+            }
         </Routes>
     )
 }
